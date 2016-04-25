239f25c635397b095bef0a1e9e8ce3d9d7dab633
miguel <miganrobles@gmail.com>
2016-04-25 20:36:35 +0200

    0122: ahora el jugador usa comandos en italiano

--- CommandWords.java
+++ CommandWords.java
@@ -26,8 +26,8 @@ public class CommandWords
         Option[] opciones = Option.values();
         comandos = new HashMap<>();
         final String[] validCommands = {
-                "go", "quit", "help", "look", "eat", "back",
-                "take", "drop", "items"};
+                "andare", "smettere", "aiuto", "guarda", "mangiare", "indietro",
+                "prendere", "far cadere", "elementi"};
         for (int i = 0; i < validCommands.length; i++) {
             comandos.put(validCommands[i], opciones[i]);
         }
